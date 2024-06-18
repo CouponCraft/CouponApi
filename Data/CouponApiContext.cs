e3e47e5cbf787fc81bd1115747486d0b4afd9cf2
KennyVell <santiagocherrys@hotmail.com>
2024-06-17 20:28:59 -0500

    Context set

--- Data/CouponApiContext.cs
+++ Data/CouponApiContext.cs
@@ -10,5 +10,14 @@ namespace CouponApi.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Coupon> Coupons { get; set; }
         public DbSet<CouponHistory> CouponHistories { get; set; }
+        public DbSet<CouponUsage> CouponUsages { get; set; }
+        public DbSet<MarketingUser> MarketingUsers { get; set; }
+        public DbSet<MarketplaceUser> MarketplaceUsers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<PurchaseCoupon> PurchaseCoupons { get; set; }
+        public DbSet<PurchaseProduct> PurchaseProducts { get; set; }
+        public DbSet<Role> Roles { get; set; } 
+        public DbSet<UserRole> UserRoles { get; set; }
     }
 }
